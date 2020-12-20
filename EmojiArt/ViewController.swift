017d46cd6ec20c306f3c421401deec830820bc75
Bolotov Alexandr <projectfebr@gmail.com>
2020-12-20 20:18:26 +0300

    Initial Commit

---
+++ EmojiArt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmojiArt
+//
+//  Created by Александр Болотов on 20.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
